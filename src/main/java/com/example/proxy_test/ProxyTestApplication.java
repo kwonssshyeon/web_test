09e63737a4decf0848e1259f5ee9bdc7298f04ad
kwonssshyeon <kwonsh4396@gmail.com>
2024-01-10 18:10:26 +0900

    initial commit

---
+++ src/main/java/com/example/proxy_test/ProxyTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.proxy_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyTestApplication.class, args);
+	}
+
+}
