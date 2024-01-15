daea1d217e186d178ad45c2a6ef975e7a7b51328
kwonssshyeon <kwonsh4396@gmail.com>
2024-01-15 23:24:03 +0900

    다른서버로 get요청 보내기

--- src/main/java/com/example/proxy_test/controller/TestController.java
+++ src/main/java/com/example/proxy_test/controller/TestController.java
@@ -2,6 +2,7 @@ package com.example.proxy_test.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 
 @RestController
@@ -14,6 +15,15 @@ public class TestController {
     @GetMapping("/1")
     public String one(){
         return "1번 서버용 메소드";
+    }
+
+    @GetMapping("/jump")
+    public String junp(){
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "http://52.79.61.197:8080/test";
+
+        String responseBody = restTemplate.getForObject(url, String.class);
+        return responseBody.toString();
     }
 
 }
